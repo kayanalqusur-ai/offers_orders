5f12e19f0c1b12fa3fd222c262a4aa7318935de6
kayanalqusur-ai <kayanalqusur@gmail.com>
2025-09-05 05:54:49 +0300

    اضافة مجلدلت

--- main.py
+++ main.py
@@ -1016,13 +1016,14 @@ with app.app_context():
             password=generate_password_hash("admin123")
         )
         admin.set_permissions([
-            'logs_view', 'list_employees', 'add_employee', 'edit_employee', 'delete_employee',
-            'rentalm_offers_view', 'rentalm_offers_add', 'rentalm_offers_edit', 'rentalm_offers_delete',
-            'rentalw_offers_view', 'rentalw_offers_add', 'rentalw_offers_edit', 'rentalw_offers_delete',
-            'salesm_offers_view', 'salesm_offers_add', 'salesm_offers_edit', 'salesm_offers_delete',
-            'salesw_offers_view', 'salesw_offers_add', 'salesw_offers_edit', 'salesw_offers_delete',
-            'orders_view', 'orders_add', 'orders_edit', 'orders_delete'
-        ])
+        'logs_view', 'list_employees', 'add_employee', 'edit_employee', 'delete_employee',
+        'rentalm_offers_view', 'rentalm_offers_add', 'rentalm_offers_edit', 'rentalm_offers_delete',
+        'rentalw_offers_view', 'rentalw_offers_add', 'rentalw_offers_edit', 'rentalw_offers_delete',
+        'salesm_offers_view', 'salesm_offers_add', 'salesm_offers_edit', 'salesm_offers_delete',
+        'salesw_offers_view', 'salesw_offers_add', 'salesw_offers_edit', 'salesw_offers_delete',
+        'orders_view', 'orders_add', 'orders_edit', 'orders_delete'
+      ])
+
         db.session.add(admin)
         db.session.commit()
         print("✅ تم إنشاء المستخدم الأول: admin / admin123")
